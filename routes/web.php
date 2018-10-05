18c8d723573a528695cb9088be1902e6a5f96c36
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2018-10-05 12:53:57 +0800

    :pencil: 將xampp的route路徑移除

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$prefix_url = 'test-project/public/';
+
 Route::get('/', 'ContestController@getRule');
 Route::get('error', 'ContestController@getError');
 Route::get('rule', 'ContestController@getRule');
@@ -19,11 +19,3 @@ Route::get('import', 'ContestController@getImport');
 Route::post('import', 'ContestController@postImport');
 Route::get('draw', 'ContestController@getDraw');
 Route::post('draw', 'ContestController@postDraw');
-
-Route::get($prefix_url.'error', 'ContestController@getError');
-Route::get($prefix_url.'rule', 'ContestController@getRule');
-Route::post($prefix_url.'rule', 'ContestController@postRule');
-Route::get($prefix_url.'import', 'ContestController@getImport');
-Route::post($prefix_url.'import', 'ContestController@postImport');
-Route::get($prefix_url.'draw', 'ContestController@getDraw');
-Route::post($prefix_url.'draw', 'ContestController@postDraw');
