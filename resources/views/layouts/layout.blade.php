4baddcd7df177d5fa022b74765d3670e9c9ae4b2
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2019-04-26 22:11:10 +0800

    :wrench: 在HTML的head加入CSRF Token、修改axios header傳出時代入CSRF Token

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <link rel="icon" href="/image/favicon.png">
         <title>InduLab</title>
 
