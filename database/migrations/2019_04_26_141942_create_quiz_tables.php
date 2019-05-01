1cd7a0242997bae8f7364cf1526ec179f9366b14
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2019-05-01 21:32:39 +0800

    :wrench: 註冊用戶加入學號遷移、問題題庫資料表遷移綁定外鍵

--- database/migrations/2019_04_26_141942_create_quiz_tables.php
+++ database/migrations/2019_04_26_141942_create_quiz_tables.php
@@ -24,7 +24,8 @@ class CreateQuizTables extends Migration
         // 創建測驗問題資料表
         Schema::create('quiz_question', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->integer('qd_id')->references('id')->on('quiz_db')->comment('對應到quiz_db的id');
+            $table->integer('qd_id')->unsigned();
+            $table->foreign('qd_id')->references('id')->on('quiz_db')->onDelete('cascade')->onUpdate('cascade')->comment('對應到quiz_db的id');
             $table->text('qs_text')->comment('問題題目內容');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
@@ -33,7 +34,8 @@ class CreateQuizTables extends Migration
         // 創建測驗問題選項資料表
         Schema::create('quiz_choice', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->integer('qq_id')->references('id')->on('quiz_question')->comment('對應到quiz_question的id');
+            $table->integer('qq_id')->unsigned();
+            $table->foreign('qq_id')->references('id')->on('quiz_question')->onDelete('cascade')->onUpdate('cascade')->comment('對應到quiz_question的id');
             $table->text('ans_text')->comment('選項答案內容');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
@@ -42,8 +44,10 @@ class CreateQuizTables extends Migration
         // 創建測驗答案資料表
         Schema::create('quiz_answer', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            $table->integer('qq_id')->references('id')->on('quiz_question')->comment('對應到quiz_question的id');
-            $table->integer('qc_id')->references('id')->on('quiz_choice')->comment('對應到quiz_choice的id，表示選項內的正確答案');
+            $table->integer('qq_id')->unsigned();
+            $table->integer('qc_id')->unsigned();
+            $table->foreign('qq_id')->references('id')->on('quiz_question')->onDelete('cascade')->onUpdate('cascade')->comment('對應到quiz_question的id');
+            $table->foreign('qc_id')->references('id')->on('quiz_choice')->onDelete('cascade')->onUpdate('cascade')->comment('對應到quiz_choice的id，表示選項內的正確答案');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
@@ -56,9 +60,9 @@ class CreateQuizTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('quiz_db');
-        Schema::dropIfExists('quiz_question');
-        Schema::dropIfExists('quiz_choice');
         Schema::dropIfExists('quiz_answer');
+        Schema::dropIfExists('quiz_choice');
+        Schema::dropIfExists('quiz_question');
+        Schema::dropIfExists('quiz_db');
     }
 }
--- database/migrations/2019_04_30_194309_create_users_table.php
+++ database/migrations/2019_04_30_194309_create_users_table.php
@@ -16,7 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('student_id')->unique();
+            $table->string('email')->unique()->nullable(TRUE);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
