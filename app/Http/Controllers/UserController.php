24cd0b42baef83d17571b8a2afac3e1389bbeb66
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2019-05-06 14:14:04 +0800

    :bug: 修正繞過驗證當前輸入的問題

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -140,7 +140,7 @@ class UserController extends Controller
             'student_id' => 'sometimes|string|max:255|unique:users,student_id,'.$user->id,
             // 忽略當前列的使用者，但檢查其他欄位的唯一性。
             'email' => 'sometimes|string|email|max:255|unique:users,email,'.$user->id,
-            'password' => 'required_with:new_password|sometimes|string|check_password:'.$user->id,
+            'password' => 'nullable|required_with:new_password,new_password_confirmation|string|check_password:'.$user->id,
             'new_password' => 'required_with:password|string|min:4|max:255|confirmed',
         ];
 
